cfb2774ab12018b39991510adf164a1de8cde40e
leewonjae <nurinamu@gmail.com>
2013-10-15 21:35:02 +0900

    ng-grid div를 ng-excel directive로 전환함.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,12 +2,29 @@
 
 angular.module('ngGridApp', ['ngGrid'])
 	.controller('MainCtrl', function($scope){
-		$scope.myData = [{name:'nurintmau', contact:'01093124732'}];
-		$scope.gridOptions = {data:'myData'};
+		$scope.myData = [];
+		//$scope.gridOptions = { data:'myData'};
 
 		$scope.addContact = function(){
 			$scope.myData.push({name:$scope.newName, contact:$scope.newContact});
 		}
+	}).directive('ngExcel',function(){
+		console.log('test');
+		return {
+			template: "<div ng-grid=\"ngExcelIntData\"></div>",
+			restrict : "EA",
+			scope: {
+				ngExcelData : "="
+			},
+			controller : function($scope){
+				//$scope.myData = [{name:'nurintmau', contact:'01093124732'}];
+				$scope.ngExcelIntData = {data:'ngExcelData'};
+			},
+			link : function($scope, iEl,iAt,ctrl){
+				$scope.$watch('ngExcelData',function(newData){
+					console.log(newData);
+				},true);
+
+			}
+		};
 	});
-
-
