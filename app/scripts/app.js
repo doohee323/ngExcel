4969f6a27e64d22225a9b336731ed53183b07019
Dewey Hong <doohee323@gmail.com>
2013-10-23 13:48:34 +0900

    dynamic grid init

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,29 +2,50 @@
 
 angular.module('ngGridApp', ['ngGrid'])
 	.controller('MainCtrl', function($scope){
-		$scope.myData = [];
-		//$scope.gridOptions = { data:'myData'};
-
+		$scope.ngExcelData = [{name: "Moroni", age: 50},
+	                     {name: "Tiancum", age: 43},
+	                     {name: "Jacob", age: 27},
+	                     {name: "Nephi", age: 29},
+	                     {name: "Enos", age: 34}];
 		$scope.addContact = function(){
-			$scope.myData.push({name:$scope.newName, contact:$scope.newContact});
+			$scope.ngExcelData.push({name:$scope.newName, contact:$scope.newContact});
 		}
-	}).directive('ngExcel',function(){
-		console.log('test');
+	})
+	.directive('ngExcel', function($compile){
 		return {
-			template: "<div ng-grid=\"ngExcelIntData\"></div>",
+			template: "<div ng-grid=\"gridOptions\" ng-style=\"myprop()\" class=\"well\"></div>",
 			restrict : "EA",
-			scope: {
-				ngExcelData : "="
-			},
 			controller : function($scope){
-				//$scope.myData = [{name:'nurintmau', contact:'01093124732'}];
-				$scope.ngExcelIntData = {data:'ngExcelData'};
+				$scope.gridOptions = {
+			        data: 'ngExcelData',
+			        multiSelect: false,  
+			        enableCellSelection: true,
+			        enableRowSelection: true,
+			        enableSorting: true,
+			        columnDefs: [
+			        {field:'name', displayName:'name', enableCellEdit: true},
+			        {field:'contact', displayName:'contact', enableCellEdit: true}
+			        ],
+			        selectedItems: []
+			    };
 			},
-			link : function($scope, iEl,iAt,ctrl){
-				$scope.$watch('ngExcelData',function(newData){
+			link : function(scope, element, attr, ctrl){
+				scope.gridOptions.attr = attr;
+				scope.$watch('ngExcelData', function(newData){
+					scope.myprop = function() {
+						var attr = scope.gridOptions.attr;
+						if(attr) {
+				            return {
+				                width: attr["width"] + 'px',
+				                height: attr["height"] + 'px'
+				            };
+						} else {
+							return {};
+						}
+			        };
 					console.log(newData);
 				},true);
-
+				// $compile(iEl.contents())(scope);
 			}
 		};
 	});
